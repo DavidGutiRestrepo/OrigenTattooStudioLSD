1f26f3946443cf7e4b6e1f2f663bd4165f25a717
DavidGutiRestrepo <Dgutierrez977@misena.edu.co>
2023-09-30 11:57:30 -0500

    Editar y revisar update y show en user y tattoo

--- backend/controller/tattoo.controller.js
+++ backend/controller/tattoo.controller.js
@@ -32,7 +32,80 @@ exports.createTattooController = async (req, res) => {
       });
     }
   } catch (error) {
-    console.log(error);
+    console.log(error);const tattooUseCases = require('../data-access/tattoos.data');
+
+    exports.showTattooController = async (req, res) => {
+      try {
+        const result = await tattooUseCases.showTattoos();
+    
+        if (result.error) {
+          return res.json({
+            error: result.error,
+          });
+        } else if (result.success) {
+          return res.json({
+            success: result.success,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    exports.createTattooController = async (req, res) => {
+      try {
+        const result = await tattooUseCases.createTattoo(req.body);
+    
+        if (result.error) {
+          return res.json({
+            error: result.error,
+          });
+        } else if (result.success) {
+          return res.json({
+            success: result.success,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    exports.updateTattooController = async (req, res) => {
+      try {
+        const result = await tattooUseCases.updateTattoo(req.body);
+    
+        if (result.error) {
+          return res.json({
+            error: result.error,
+          });
+        } else if (result.success) {
+          return res.json({
+            success: result.success,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    exports.deleteTattooController = async (req, res) => {
+      try {
+        const result = await tattooUseCases.deleteTattoo(req.body);
+    
+        if (result.error) {
+          return res.json({
+            error: result.error,
+          });
+        } else if (result.success) {
+          return res.json({
+            success: result.success,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
   }
 };
 
