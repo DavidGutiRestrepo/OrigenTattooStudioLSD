ec182d0c915049030202131a173f3a373419cd38
DavidGutiRestrepo <Dgutierrez977@misena.edu.co>
2023-10-07 11:29:09 -0500

    Terminar los demas swagger

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,17 +7,110 @@ const controllerPiercings = require('../controller/piercings.controller');
 const controllerTattoo = require('../controller/tattoo.controller');
 const controllerUser = require('../controller/users.controller');
 
+
+/**
+ * @swagger 
+ * components:
+ *  schemas:
+ *    Client:
+ *      type: object
+ *      properties: 
+ *          nombre:
+ *            type: string
+ *            description: El nombre del cliente
+ * 
+ *          apellido:
+ *             type: string
+ *             description: El apellido del cliente
+ * 
+ *          celular:
+ *           type: integer
+ *           description: El celular del cliente
+ * 
+ *          documento:
+ *           type: string
+ *           description: El documento del cliente
+ *          
+ *          correo:
+ *           type: string
+ *           description: El correo del cliente
+ *          
+ *          fechaNacimiento:
+ *           type: string
+ *           description: La fecha de nacimiento del cliente
+ * 
+ *          alergias:
+ *           type: string
+ *           description: La o las alergias del cliente
+ *          
+ *          idTatuaje: 
+ *           type: string 
+ *           description: Tatuaje pedido por el cliente
+ * 
+ *          idPiercing:
+ *           type: string
+ *           description: Piercing pedido por el cliente
+ * 
+ *          contrasena:
+ *            type: string
+ *            description: La contraseña del cliente
+ * 
+ *      required:
+ *         -nombre
+ *         -apellido
+ *         -celular
+ *         -documento
+ *         -correo
+ *         -fechaNacimiento
+ *         -alergias
+ *         -idTatuaje
+ *         -idPiercing
+ *         -contrasena
+ *      example:
+ *        nombre: David
+ *        apellido: Gutierrez 
+ *        celular: 3147746834
+ *        documento: 1001362779
+ *        correo: dprince@gmail.com
+ *        fechaNacimiento: 04/03/2002
+ *        alergias: Ninguna
+ *        idTatuaje: 0
+ *        idPiercing: 1
+ *        contrasena: 10123456  
+ *           
+ */
+
+/**
+ * @swagger
+ * /api/v1/:
+ *  post:
+ *      sumamry: create a new client
+ *      tags: [Client]
+ *      requestBody:
+ *          required: true
+ *          content:    
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                $ref: '#/components/schemas/Client'
+ *      responses:
+ *          200:
+ *            description: Nuevo usuario creador!
+ */
 //client
 router.get('/showclient', controllerClients.showClientController);
 router.post('/newclient', controllerClients.createClientController);
 router.post('/updateclient', controllerClients.updateClientController);
 router.delete('/deleteclient', controllerClients.deleteClientController);
 
+
+
 // tattoo
 router.get('/showtattoo', controllerTattoo.showTattooController);
 router.post('/newtattoo', controllerTattoo.createTattooController);
 router.post('/updatetattoo', controllerTattoo.updateTattooController);
 router.delete('/deletetattoo', controllerTattoo.deleteTattooController);
+
 
 // users
 router.get('/showuser', controllerUser.showUserController);
--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require('express');
 const app = express();
 const routes = require('./backend/routes/routes');
 require('dotenv').config();
+const path=require('path')
+const swaggerUI=require('swagger-ui-express');
+const swaggerJsDoc=require('swagger-jsdoc');
+const swaggerSpec={
+  definition:{
+    openapi:'3.1.0',
+    info:{
+      title:'OrigenTattoStudio',
+      version:'1.0.0'
+    },
+    servers:[
+      {
+      url:'http://localhost:7012'
+      }]
+  },
+  apis:[ `${path.join(__dirname,"./backend/routes/routes.js")}`],
+  };
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api/v1/', routes);
 
+app.use('/api/v1/', routes);
+app.use('/api.doc',swaggerUI.serve,swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
 app.listen(process.env.PORT, () => {
   console.log('funciona');
 });
